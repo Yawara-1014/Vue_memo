3eb38adf3fd698a4e887409ef70428657f38909b
Yawara-1014 <urumajin@gmail.comm>
2020-05-03 23:44:07 +0900

    add_remove

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="editor">
+    <Header>Edit memos</Header>
     <textarea name="memo" v-model="memoBody"></textarea>
     <button @click="save">保存</button>
-    <button class="remove">削除</button>
+    <button class="remove" @click="remove">削除</button>
   </div>
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
+
 export default {
   name: "edit",
+  components: {
+    Header
+  },
   data: function(){
     return {
       memoBody: ''
@@ -31,6 +37,10 @@ export default {
         id: this.$route.params["id"],
         body: this.memoBody
       });
+      this.$router.push("/");
+    },
+    remove: function(){
+      this.$store.commit("remove", this.$route.params["id"]);
       this.$router.push("/");
     }
   }
