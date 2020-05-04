e84be6411e30cd73f0c03ad7effad54e3072eb71
Yawara-1014 <urumajin@gmail.comm>
2020-05-04 11:20:05 +0900

    add_deploy_reademe

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# notepad
+<img width="846" alt="スクリーンショット 2020-05-04 0 12 57" src="https://user-images.githubusercontent.com/57986669/80932074-a782e280-8df8-11ea-8600-3bcc44b45825.png">
+
+<h2 align="center">Vue_memo</h2>
+
+<p align="center">
+  <a href="vue公式サイトURL"><img src="https://〜.png" width="80px;" /></a>
+</p>
+
+## 🌐 App URL
+
+### **https://immense-everglades-67145.herokuapp.com/**  
+
+## アプリ概要
+Vue.jsをメイン言語に使用したメモアプリになります
 
 ## Project setup
 ```
---
+++ dist/js/about.fa0b4438.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("p",[e._v("Home にて投稿したメモが見れます。")]),n("p",[e._v("Home にて投稿したメモをクリックすることで編集・削除ができます。")]),n("p",[e._v("New にて新規でメモを投稿することができます。")])])}],u=n("2877"),c={},l=Object(u["a"])(c,a,s,!1,null,null,null);t["default"]=l.exports}}]);
+//# sourceMappingURL=about.fa0b4438.js.map
